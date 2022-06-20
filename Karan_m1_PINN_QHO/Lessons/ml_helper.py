6e5b042a7567ec75c34862ea2b0363b9bdd01a64
karanprime <k.shah@hzdr.de>
2022-06-20 14:21:10 -0400

    Cleaned up repo, updated module

---
+++ Karan_m1_PINN_QHO/Lessons/ml_helper.py
@@ -0,0 +1,320 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+from ipywidgets import interact
+
+
+
+def viz_colour_rgb(c):
+    labels = ['r','g','b','','col']
+    
+    w = [1,1,1]
+    cb = np.zeros((1,5,3))
+    cb[0,0,0] = c[0]
+    cb[0,1,1] = c[1]
+    cb[0,2,2] = c[2]
+    cb[0,3,:] = w
+    cb[0,4,:] = c
+
+   
+    fig = plt.figure(figsize = (5,10))
+    ax = fig.add_axes([0,0, 1, 1])  # span the whole figure
+    #ax.set_axis_off()
+    
+    ax.set_xticks(np.arange(len(labels)), labels=labels)
+#    ax.set_yticks(np.arange(len(vegetables)), labels=vegetables)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=0, ha="left",
+             rotation_mode="anchor")
+
+
+    ax.tick_params(left = False, right = False , labelleft = False ,
+                    labelbottom = True, bottom = False)
+    #ax.xaxis.set_major_formatter(ticker.FixedFormatter(labels))
+    #ax.set_xticklabels(labels, rotation=0, ha='left', minor=False)
+ 
+    ax.imshow(cb)
+    plt.show()
+    print(f"r = {float(c[0]):.2}, g = {float(c[1]):.2}, b = {float(c[2]):.2}")
+
+def viz_colour_predicted_rgb(t,p):
+    print("Input RGB:")
+    viz_colour_rgb(t)
+    print("\nOutput Prediction RGB:")
+    viz_colour_rgb(p)
+    print("##############################")
+
+# def viz_colour_predicted(t,p):
+#     labels = ['r','g','b','','t','','p']
+    
+#     w = [1,1,1]
+#     cb = np.zeros((1,7,3))
+#     cb[0,0,0] = t[0]
+#     cb[0,1,1] = t[1]
+#     cb[0,2,2] = t[2]
+#     cb[0,3,:] = w
+#     cb[0,4,:] = t
+#     cb[0,5,:] = w
+#     cb[0,6,:] = p
+
+   
+#     fig = plt.figure(figsize = (5,10))
+#     ax = fig.add_axes([0, 0, 1, 1])  # span the whole figure
+#     #ax.set_axis_off()
+    
+#     ax.set_xticks(np.arange(len(labels)), labels=labels)
+# #    ax.set_yticks(np.arange(len(vegetables)), labels=vegetables)
+
+#     # Rotate the tick labels and set their alignment.
+#     plt.setp(ax.get_xticklabels(), rotation=0, ha="left",
+#              rotation_mode="anchor")
+
+
+#     ax.tick_params(left = False, right = False , labelleft = False ,
+#                     labelbottom = True, bottom = False)
+#     #ax.xaxis.set_major_formatter(ticker.FixedFormatter(labels))
+#     #ax.set_xticklabels(labels, rotation=0, ha='left', minor=False)
+#     ax.imshow(cb)
+#     plt.show()
+#     print(f"True: r = {t[0]:.2}, g = {t[1]:.2}, b = {t[2]:.2}")
+#     print(f"Predited: r = {p[0]:.2}, g = {p[1]:.2}, b = {p[2]:.2}")
+
+rgb_scale = 1.0
+cmyk_scale = 1.0
+
+def rgb_to_cmyk(col):
+    r,g,b = col
+    if (r == 0) and (g == 0) and (b == 0):
+        # black
+        return 0, 0, 0, cmyk_scale
+
+    # rgb [0,1] -> cmy [0,1]
+    c = 1 - r 
+    m = 1 - g
+    y = 1 - b
+
+    # extract out k [0,1]
+    min_cmy = min(c, m, y)
+    c = (c - min_cmy) 
+    m = (m - min_cmy) 
+    y = (y - min_cmy) 
+    k = min_cmy
+
+    # rescale to the range [0,cmyk_scale]
+    return c*cmyk_scale, m*cmyk_scale, y*cmyk_scale, k*cmyk_scale
+
+def cmyk_to_rgb(col):
+    """
+    """
+    c,m,y,k = col
+    r = max(1.0-(c+k),0.0)
+    g = max(1.0-(m+k),0.0)
+    b = max(1.0-(y+k),0.0)
+    return r,g,b
+
+def viz_colour_cmyk(c):
+    labels = ['c','m','y','k','','col']
+    c = np.asarray(c)
+    
+    cyan = np.asarray([0,1.0,1.0])
+    magenta = np.asarray([1.0,0.0,1.0])
+    yellow = np.asarray([1.0,1.0,0.0])
+    w = np.asarray([1,1,1])
+    
+    cyan = np.asarray([1.0,0,0,0])
+    magenta = np.asarray([0,1,0,0])
+    yellow = np.asarray([0,0,1,0])
+    w = np.asarray([1,1,1])
+    
+    
+    rgb_c = cmyk_to_rgb(c)
+    cb = np.zeros((1,6,3))
+    cb[0,0,:] = cmyk_to_rgb(c[0] * cyan)
+    cb[0,1,:] = cmyk_to_rgb(c[1] * magenta)
+    cb[0,2,:] = cmyk_to_rgb(c[2] * yellow)
+    cb[0,3,:] = (1 - c[3]) * w
+    cb[0,4,:] = w
+    cb[0,5,:] = rgb_c
+
+   
+    fig = plt.figure(figsize = (5,10))
+    ax = fig.add_axes([0,0, 1, 1])  # span the whole figure
+    #ax.set_axis_off()
+    
+    ax.set_xticks(np.arange(len(labels)), labels=labels)
+#    ax.set_yticks(np.arange(len(vegetables)), labels=vegetables)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=0, ha="left",
+             rotation_mode="anchor")
+
+
+    ax.tick_params(left = False, right = False , labelleft = False ,
+                    labelbottom = True, bottom = False)
+    #ax.xaxis.set_major_formatter(ticker.FixedFormatter(labels))
+    #ax.set_xticklabels(labels, rotation=0, ha='left', minor=False)
+ 
+    ax.imshow(cb)
+    plt.show()
+    #print(f"r = {float(rgb_c[0]):.2}, g = {float(rgb_c[1]):.2}, b = {float(rgb_c[2]):.2}")
+    print(f"c = {float(c[0]):.2}, m = {float(c[1]):.2}, y = {float(c[2]):.2}, k = {float(c[3]):.2}")
+
+def viz_colour_predicted_cmyk(t,p):
+    print("Input RGB:")
+    viz_colour_rgb(t)
+    print("\nOutput Prediction:")
+    viz_colour_cmyk(p)
+    print("##############################")
+
+class ColourDataset(Dataset):
+
+    def __init__(self, input_data, output_data, transform=None):
+        
+        self.input_data = input_data
+        self.output_data = output_data
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.input_data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        sample = [self.input_data[idx], self.output_data[idx]]
+
+
+        return sample
+debug = False
+
+def get_plots_norm_colorbar(y_true,y_pred, n_x, n_t, x_dom, t_dom, plot_fname):
+    labels = ["Predicted", "True", "Abs Err"]
+    
+    u,v,dens = get_density(y_true.detach().numpy())
+    u_pred, v_pred, dens_pred = get_density(y_pred.detach().numpy())
+    
+    
+
+    plot_name = 'TD SE Results' #remove 'network_checkpoint_'
+    fig = plt.figure(figsize=(7,8),dpi=100)
+    
+    mse_u = 0.0
+    mse_v = 0.0
+    show_cb = False
+    cmap = plt.get_cmap('viridis')
+    
+    x_min = 0
+    x_max = 0
+    t_min = 0
+    t_max = 0
+    
+    
+    u_max = -99.
+    u_min = 99.
+    v_max = -99.
+    v_min = 99.
+    d_max = -99.
+    d_min = 99.
+    
+    u_max = np.max((np.max(u),np.max(u_pred)))
+    u_min = np.min((np.min(u),np.min(u_pred)))
+    v_max = np.max((np.max(v),np.max(v_pred)))
+    v_min = np.min((np.min(v),np.min(v_pred)))
+    d_max = np.max((np.max(dens),np.max(dens_pred)))
+    d_min = np.min((np.min(dens),np.min(dens_pred)))
+        
+    extent = [x_dom[0] , x_dom[1], t_dom[0] ,t_dom[1]]
+    
+    u_plot = np.zeros((n_x,n_t))
+    v_plot = np.zeros_like((n_x,n_t))
+    d_plot = np.zeros_like((n_x,n_t))
+    
+    reshape_to_grid = lambda x: np.reshape(x,  (n_x, n_t))
+    
+    for i in np.arange(3):
+        if i==0:
+            u_plot = reshape_to_grid(u_pred)
+            v_plot = reshape_to_grid(v_pred)
+            d_plot = reshape_to_grid(dens_pred)
+            show_cb = False
+        if i==1:
+            u_plot = reshape_to_grid(u)
+            v_plot = reshape_to_grid(v)
+            d_plot = reshape_to_grid(dens)
+        if i == 2:
+            u_plot = np.abs(reshape_to_grid(u - u_pred))
+            v_plot = np.abs(reshape_to_grid(v - v_pred))
+            d_plot = np.abs(reshape_to_grid(dens_pred - dens))
+            show_cb = True
+
+
+        ax1 = fig.add_subplot(3,3,i+1)
+        ims = ax1.imshow(u_plot, extent=extent,vmin=u_min, vmax=u_max)
+        ax1.set_title(fr"{labels[i]} Re($\psi$)")
+        ax1.set_xlabel(r'$x$')
+        ax1.set_ylabel(r'$t$')
+        if show_cb:
+            divider = make_axes_locatable(ax1)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            norm = mpl.colors.Normalize(vmin=u_min, vmax=u_max)
+            sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+            sm.set_array([])
+            fig.colorbar(sm, cax=cax)
+            
+        ax1 = fig.add_subplot(3,3,i+4)
+        ax1.set_title(fr"{labels[i]} Im($\psi$)")
+        ims = ax1.imshow(v_plot, extent=extent,vmin=v_min, vmax=v_max)
+        ax1.set_xlabel(r'$x$')
+        ax1.set_ylabel(r'$t$')
+        if show_cb:
+            divider = make_axes_locatable(ax1)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            norm = mpl.colors.Normalize(vmin=v_min, vmax=v_max)
+            sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+            sm.set_array([])
+            fig.colorbar(sm, cax=cax)
+            
+        ax1 = fig.add_subplot(3,3,i+7)
+        ax1.set_title(fr"{labels[i]} $|\psi|^2$")
+        ims = ax1.imshow(d_plot, extent=extent,vmin=d_min, vmax=d_max)
+        ax1.set_xlabel(r'$x$')
+        ax1.set_ylabel(r'$t$')
+        if show_cb:
+            divider = make_axes_locatable(ax1)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            norm = mpl.colors.Normalize(vmin=d_min, vmax=d_max)
+            sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+            sm.set_array([])
+            fig.colorbar(sm, cax=cax)
+        
+        if i==2:
+            mse_u = np.mean(u_plot**2)
+            mse_v = np.mean(v_plot**2)
+            
+        print(f"{i} finished")
+            
+    plot_title = plot_name
+    plt.suptitle(fr"{plot_title}",y=0.92)
+    plt.savefig(f'plots/{plot_fname}')
+    plt.show()
+    print(f"mse: u: {mse_u}, v:{mse_v}")
+    return mse_u,mse_v
+
+def get_density(y_pred):
+# Helper function to split data into real, imag, density
+    u = y_pred[:,0]
+    v = y_pred[:,1]
+    dens = u**2 + v**2
+    
+    return u,v,dens
+
---
+++ Karan_m1_PINN_QHO/README.md
@@ -0,0 +1,51 @@
+# Module 1: Learning the Schrödinger Equation
+Karan Shah
+
+-  **A summary**: This module can be used can be used for a Quantum Mechanics course. The module will begin with introduction to the time dependent Schrödinger Equation including a tutorial on analytical solutions the  Quantum Harmonic Oscillator etc. The second portion of this module will consist of a solution of this system using neural networks. Students are encouraged to try different parameters and modify the code to fit other systems.
+
+The module consists of following submodules:
+- 00: A gentle interactive introduction to deep learning with a simple toy problem. Students will learn how to create neural network pipelines and develop qualitative understanding.
+- 01: A broad introduction to different concepts in machine learning, with a focus on applications in physics. Numerous references are included for students to explore further.
+- 02: An interactive notebook on solving 1D Quantum Harmonic Oscillator, consisting of two states in superposition evolving in time.
+
+![Time evolution for a 3D Quantum Harmonic Oscillator](Lessons/fig/3d_qho.png)
+	
+-   **What is included in this module?**: The following topics are expected:
+    -   Lecture materials and hands-on lessons
+    -   Exercises
+    -   Homework/Project
+-   **Which course(s) might these modules plug into?**
+	-   Quantum Mechanics
+-   **Physics and the data science learning goal(s):**
+	-   Physics: The Schrödinger Equation (SE) is an important part of any Quantum Mechanics course and students are expected to solve it for increasingly complex systems throughout the course. In this module, I introduce Physics-Informed Neural Networks (PINNs) and demonstrate the effectiveness of numerical techniques and deep learning for solving the SE for Quantum Harmonic Oscillators. A good time to present this module would be after students solve the Time Independent Schrodinger Equation for QHOs and are being introduced to time dependent systems.
+
+- Data Science: PINNs [1,2] are a novel class of machine learning algorithms for solution of partial differential equations. This is achieved by incorporating structured prior information derived from physical laws into the learning algorithm. PINNs are constructed by encoding the constraints posed by a given differential equation and its boundary conditions into the loss function of a NN. This constraint guides the network to approximate the solution of the differential equation.
+
+![PINN architecture](Lessons/fig/PINN_diagrams.png)
+
+-   **Estimated amount of time these might take a student to complete**: 6 hours
+-   **Estimated amount of time these might take a professor to teach**: 2 hours
+-   **Pre-requisites:** 
+    - [Core prerequisites](https://github.com/GDS-Education-Community-of-Practice/DSECOP/wiki/Core-prerequisites)
+    - Intro to Deep Learning Module by Fatemeh Bagheri
+
+***Instructions for setting up the environment***
+To set up the environment with conda:
+
+Create new env:
+`conda create -n dsecop_m1 python=3.8`
+
+Activate the env:
+`conda activate dsecop_m1`
+
+Install required libraries:
+```
+conda install numpy matplotlib jupyter ipywidgets
+conda install pytorch torchvision cpuonly -c pytorch
+```
+
+Run `jupyter notebook` in this directory. A browser window will open where the notebooks can be viewed.
+
+-   **References:**
+    1. M. Raissi, P. Perdikaris, and G. E. Karniadakis. Physics-informed neural networks: A deep learning framework for solving forward and inverse problems involving nonlinear partial differential equations. Journal of Computational Physics, 378:686–707, February 2019.
+    2. George Em Karniadakis, Ioannis G. Kevrekidis, Lu Lu, Paris Perdikaris, Sifan Wang, and Liu Yang. Physics-informed machine learning. Nature Reviews Physics, 3(6):422–440, June 2021
